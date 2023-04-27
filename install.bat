f4a080dff6cb7a9ea5d3f218e573285199b25c3b
Zachary Van De Staey <zacharyplays2.0@gmail.com>
2023-04-27 18:28:10 +0200

    first commit

---
+++ install.bat
@@ -0,0 +1,6 @@
+echo "starting install of the old context menu.";
+reg add "HKCU\Software\Classes\CLSID\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}\inprocServer32" /f /ve
+
+echo "------------------------------";
+echo "Old context menu key has been added. Please restart the computer."
+echo "------------------------------";
