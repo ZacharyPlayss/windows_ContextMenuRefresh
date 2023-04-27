d27c2457949491b6794b1672606f1321ba8ec213
Zachary Van De Staey <zacharyplays2.0@gmail.com>
2023-04-27 18:40:30 +0200

    Text aanpassingen

--- install.bat
+++ install.bat
@@ -1,9 +1,9 @@
 @echo off
 echo "------------------------------"
-echo "starting install of the old context menu."
+echo "Installatie starten van het oude context menu."
 reg add "HKCU\Software\Classes\CLSID\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}\inprocServer32" /f /ve
 
 echo "------------------------------"
-echo "Old context menu key has been added."
+echo "De locale sleutel voor het context menu werd succesvol aangemaakt."
 echo "------------------------------"
-echo "Please restart the computer!"
+echo "Herstart het systeem om met succes de installatie af te handelen."
--- remove.bat
+++ remove.bat
@@ -1,9 +1,9 @@
 @echo off
 echo "------------------------------"
-echo "starting removal of old context menu. This will reset it to windows 11 standards."
+echo "De locale sleutel voor het herstelen van het windows 11 context menu wordt gezocht en verwijderd."
 reg delete "HKCU\Software\Classes\CLSID\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}" /f
 
 echo "------------------------------"
-echo "Old context menu key removed succesfully."
+echo "De sleutel voor het oude context menu werd met succes verwijderd."
 echo "------------------------------"
-echo "restart the computer please!"
+echo "Herstart het systeem om de herstelling te vervolledigen."
