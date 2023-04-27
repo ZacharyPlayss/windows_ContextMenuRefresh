8654ac3bd2a560d2904b1b2e88a368366c2eb7a4
Zachary Van De Staey <zacharyplays2.0@gmail.com>
2023-04-27 18:36:40 +0200

    fixed echo's withing the files.

--- remove.bat
+++ remove.bat
@@ -1,6 +1,9 @@
+@echo off
+echo "------------------------------"
 echo "starting removal of old context menu. This will reset it to windows 11 standards."
 reg delete "HKCU\Software\Classes\CLSID\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}" /f
 
 echo "------------------------------"
-echo "Old context menu key has been added. Please restart the computer."
+echo "Old context menu key removed succesfully."
 echo "------------------------------"
+echo "restart the computer please!"
