bbd5962ec8115df4dc8a05184d059946f8bc804a
Zachary Van De Staey <zacharyplays2.0@gmail.com>
2023-04-27 18:37:43 +0200

    fixed echo's in install bat file.

--- install.bat
+++ install.bat
@@ -1,9 +1,9 @@
 @echo off
-echo "------------------------------";
-echo "starting install of the old context menu.";
+echo "------------------------------"
+echo "starting install of the old context menu."
 reg add "HKCU\Software\Classes\CLSID\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}\inprocServer32" /f /ve
 
-echo "------------------------------";
+echo "------------------------------"
 echo "Old context menu key has been added."
-echo "------------------------------";
-echo "Please restart the computer!";
+echo "------------------------------"
+echo "Please restart the computer!"
